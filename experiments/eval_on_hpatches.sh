63e058b3f213f6050b53498ca64b66ce830c6bc3
Melekhov Iaroslav <yaroslav.melekhov@gmail.com>
2021-12-28 13:54:33 +0200

    Finalize the test script

--- experiments/eval_on_hpatches.sh
+++ experiments/eval_on_hpatches.sh
@@ -3,8 +3,16 @@
 DATASETS_PATH=/ssd/data_tmp/
 TASK=hpatches
 DESCRIPTOR=hndesc
+BACKBONE=caps
+SNAPSHOT_NAME=${DESCRIPTOR}_${BACKBONE}_M_st
 
 wget http://icvl.ee.ic.ac.uk/vbalnt/hpatches/hpatches-sequences-release.tar.gz -c -P ${DATASETS_PATH}/
 tar -zxvf ${DATASETS_PATH}/hpatches-sequences-release.tar.gz -C ${DATASETS_PATH}/
 
-python main.py task=${TASK} descriptor=${DESCRIPTOR} paths.datasets_home_dir=${DATASETS_PATH}
+export PYTHONPATH=$PWD/../:$PYTHONPATH
+
+python main.py task=${TASK} \
+               paths.datasets_home_dir=${DATASETS_PATH} \
+               descriptor=${DESCRIPTOR} \
+               descriptor.descriptor_params.backbone=${BACKBONE} \
+               descriptor.descriptor_params.exper_settings_name=${SNAPSHOT_NAME}
