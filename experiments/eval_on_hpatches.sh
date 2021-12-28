acc9b54323c18938b219d732b2c7bc455d8800f6
Melekhov Iaroslav <yaroslav.melekhov@gmail.com>
2021-12-28 13:38:21 +0200

    An example of evaluating on the HPatches dataset

--- experiments/eval_on_hpatches.sh
+++ experiments/eval_on_hpatches.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-PROJECT_DIR=$PWD/..
-DATASETS_PATH=/ssd/data
+DATASETS_PATH=/ssd/data_tmp/
 TASK=hpatches
 DESCRIPTOR=hndesc
 
+wget http://icvl.ee.ic.ac.uk/vbalnt/hpatches/hpatches-sequences-release.tar.gz -c -P ${DATASETS_PATH}/
+tar -zxvf ${DATASETS_PATH}/hpatches-sequences-release.tar.gz -C ${DATASETS_PATH}/
 
-python
+python main.py task=${TASK} descriptor=${DESCRIPTOR} paths.datasets_home_dir=${DATASETS_PATH}
