1f6bc9b1e3dc5949b78f67994d5a1d18fbe81f02
Melekhov Iaroslav <yaroslav.melekhov@gmail.com>
2021-12-28 13:01:58 +0200

    some updates

--- experiments/main.py
+++ experiments/main.py
@@ -1,25 +1,33 @@
 import hydra
+from pathlib import Path
 from experiments.hpatches.pipeline import HPSequenceBenchmark
-'''
+
+"""
 from experiments.localization.pipeline import VisualLocBenchmark
 from experiments.image_retrieval.pipeline import ImageRetrievalBenchmark
 from experiments.image_matching.pipeline import ImageMatchingBenchmark
-'''
+"""
 
 
 @hydra.main(config_path="configs", config_name="main")
 def main(cfg):
+    for name in cfg["paths"]:
+        if name == "project_dir":
+            cfg["paths"]["project_dir"] = str(
+                Path(hydra.utils.get_original_cwd()).parents[0]
+            )
+
     benchmark = None
-    if cfg.task.task_params.name == 'hpatches':
+    if cfg.task.task_params.name == "hpatches":
         benchmark = HPSequenceBenchmark(cfg)
-    '''
+    """
     elif cfg.task.task_params.name == 'localization':
         benchmark = VisualLocBenchmark(cfg)
     elif cfg.task.task_params.name == 'image_retrieval':
         benchmark = ImageRetrievalBenchmark(cfg)
     elif cfg.task.task_params.name == 'image_matching':
         benchmark = ImageMatchingBenchmark(cfg)
-    '''
+    """
 
     benchmark.evaluate()
 
