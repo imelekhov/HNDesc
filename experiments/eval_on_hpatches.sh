3d157b6fae0d847e3b96c06102d6de26815618f4
Melekhov Iaroslav <yaroslav.melekhov@gmail.com>
2021-12-30 12:59:21 +0200

    some updates

--- experiments/eval_on_hpatches.sh
+++ experiments/eval_on_hpatches.sh
@@ -4,7 +4,7 @@ DATASETS_PATH=/ssd/data_tmp/
 TASK=hpatches
 DESCRIPTOR=hndesc
 BACKBONE=caps
-SNAPSHOT_NAME=${DESCRIPTOR}_${BACKBONE}_M_st
+SNAPSHOT_NAME=${DESCRIPTOR}_${BACKBONE}_MP_st
 
 wget http://icvl.ee.ic.ac.uk/vbalnt/hpatches/hpatches-sequences-release.tar.gz -c -P ${DATASETS_PATH}/
 tar -zxvf ${DATASETS_PATH}/hpatches-sequences-release.tar.gz -C ${DATASETS_PATH}/
